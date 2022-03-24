cf682e010e5e9b43fdbea0533a47e5749e134800
datphamcode295 <vandatpro2000@gmail.com>
2022-03-24 20:13:21 +0700

    initial code

---
+++ index.js
@@ -0,0 +1,66 @@
+function myfunction(){
+    const token = ''
+    const userid = 0
+    const access_token = ''
+    
+  
+    var response =UrlFetchApp.fetch(`http://e-learning.hcmut.edu.vn/webservice/rest/server.php?wsfunction=core_message_get_messages&wstoken=${token}&newestfirst=1&useridto=${userid}&read=0`)
+  const subjectRegex = /<KEY name="subject">.*/g;
+  const messageRegex = /<KEY name="fullmessage">(.*?)EY>/gs;
+  const idRegex = /<KEY name="id">.*>/g;
+    var subjects = response.getContentText().match(subjectRegex)
+    var messages = response.toString().match(messageRegex)
+    var messageIds = response.toString().match(idRegex)
+  //  Logger.log(subjects)
+  
+    //sending mail
+    if(subjects){
+      for(i=0;i<subjects.length;i++){
+        var formData = {
+        access_token: access_token,
+        subject: subjects[i].substring(27,subjects[i].length-8),
+        text: decodeHTMLEntities(messages[i].substring(31,messages[i].length-15))
+        };
+        var options = {
+        'method' : 'post',
+        'contentType': 'application/json',
+        // Convert the JavaScript object to a JSON string.
+        'payload' : JSON.stringify(formData)
+        };
+        UrlFetchApp.fetch('https://postmail.invotes.com/send', options);
+        var time = Date.now()
+        var messageid = parseInt(messageIds[i].substring(22,messageIds[i].length-8));
+        var options = {
+              'method' : 'POST',
+              };
+        UrlFetchApp.fetch(`http://e-learning.hcmut.edu.vn/webservice/rest/server.php?wsfunction=core_message_mark_message_read&wstoken=${token}&messageid=${messageid}&timeread=${time}`,options)
+  
+        }
+    }
+    
+    
+  
+  
+  }
+  function decodeHTMLEntities(text) {
+      var entities = [
+          ['amp', '&'],
+          ['apos', '\''],
+          ['#x27', '\''],
+          ['#x2F', '/'],
+          ['#39', '\''],
+          ['#47', '/'],
+          ['lt', '<'],
+          ['gt', '>'],
+          ['nbsp', ' '],
+          ['quot', '"']
+      ];
+  
+      for (var i = 0, max = entities.length; i < max; ++i) 
+          text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
+  
+      return text;
+  }
+  
+  
+  
